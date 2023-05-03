18509a5b5f8c81122a26b6ffbbbf010d883e6add
LS <dltpt99@student.anu.ac.kr>
2023-05-03 15:49:10 +0900

    Write FOrm 작성중

--- src/src/components/recruit-write/RecruitWriteForm.jsx
+++ src/src/components/recruit-write/RecruitWriteForm.jsx
@@ -1,1 +1,20 @@
 import React from 'react';
+import stl from '../sign/SignForm.module.css';
+import SignTextLabel from '../sign/SignTextLabel';
+import SignTextInput from '../sign/SignTextInput';
+
+function RecruitWriteForm(props) {
+    const {titleOnChange} = props;
+    
+    return (
+        <div className={stl.signFormWrapper}>
+            <SignTextLabel labelText = {`제목`}/>
+            <SignTextInput
+                placeHolder={`제목을 입력하세요`}
+                onChange
+            />
+        </div>
+    );
+}
+
+export default RecruitWriteForm;
