e0d2fe995c861c3c6d20119a03b4f06f3a477984
LS <LS@me.nt>
2023-05-09 21:54:09 +0800

    chat 기능 수정

--- src/src/routes/MyChat.js
+++ src/src/routes/MyChat.js
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useRef, useState} from "react";
+import React, {useEffect, useState, ReactDOM} from "react";
 import {API} from "../../config";
 import SockJs from "sockjs-client";
 import StompJs from "stompjs";
@@ -6,19 +6,54 @@ import StompJs from "stompjs";
 
 function MyChat(props) {
     const formData = new FormData();
-    const [msg, setMsg] = useState();
+    const [msg, setMsg] = useState([]);
     const [myMsg, setMyMsg] = useState("");
+
+    let init = false;
+
     let sock;
-    let stomp;
+    var stomp;
 
     useEffect(() => {
-        sock = new SockJs(`${API.CHAT}`);
-        stomp = StompJs.over(sock);
-        stompConnect();
+        
+        init = true;
         return ()=>{
             DisConnect();
         }
+
     }, [])
+
+    useEffect(() => {
+        if(init) {
+            sock = new SockJs(`${API.CHAT}`);
+            stomp = StompJs.over(sock);
+            stompConnect();
+        }
+    }, [init])
+
+    const sendMessage = () => {
+        stomp.send(`${API.CHAT_SEND}`, {}, JSON.stringify({'name' : myMsg}));
+    };
+
+    const stompConnect = () => {
+        try {
+            //웹소켓 연결시 stomp에서 자동으로 connect이 되었다는것을
+            //console에 보여주는데 그것을 감추기 위한 debug
+            // stomp.debug = null;
+            stomp.connect({}, () => {
+                stomp.subscribe(
+                    `${API.GREET}`, (data) => {
+                        let message = JSON.parse(data.body).content;
+                        setMsg(prev => [...prev, message]);
+
+                        //데이터 파싱
+                    },
+                    {}
+                );
+            });
+        } catch (err){console.log(err)}
+    };
+
     const DisConnect = () => {
         try {
             stomp.debug = null;
@@ -28,42 +63,21 @@ function MyChat(props) {
         } catch (err) {
         }
     };
-    const SendMessage = (myMsg) => {
-        const data = {
-            'name' : myMsg
-        }
-        stomp.send(`${API.CHAT_SEND}`, {}, JSON.stringify(data));
-    };
-    const stompConnect = () => {
-        try {
-            stomp.debug = null;
-            //웹소켓 연결시 stomp에서 자동으로 connect이 되었다는것을
-            //console에 보여주는데 그것을 감추기 위한 debug
-            stomp.connect({}, () => {
-                stomp.subscribe(
-                    `${API.GREET}`,
-                    (data) => {
-                        setMsg(JSON.parse(data.body))
-                        //데이터 파싱
-                    },
-                    {}
-                );
-            });
-        } catch (err){}
-    };
 
-    return (
-        <>
-            {msg}
-            {myMsg}
-            <input type={"text"} onChange={(event)=>{
-                setMyMsg(event.target.value)
-            }}></input>
-            <button onClick={()=>{
-                SendMessage(myMsg)
-            }}>제출!</button>
-        </>
-    )
+    ReactDOM.render(() => {
+        return (
+            <>
+                {msg}
+                <hr></hr>
+                <input type={"text"} onChange={(event)=>{
+                    setMyMsg(event.target.value)
+                }}></input>
+                <button onClick={()=>{
+                    sendMessage()
+                }}>제출!</button>
+            </>
+        );
+    })
 }
 
 
