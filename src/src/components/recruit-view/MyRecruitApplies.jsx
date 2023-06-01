bcc339a8e6cc90dd6672cf20c01f6ea5663809ce
LS <LS@me.nt>
2023-06-02 02:08:05 +0900

    작성 내역에서 수락/거절 기능 추가

--- src/src/components/recruit-view/MyRecruitApplies.jsx
+++ src/src/components/recruit-view/MyRecruitApplies.jsx
@@ -1,34 +1,41 @@
-import React from 'react'
-import {useEffect, useState} from "react";;
+import React from 'react';
+import {useEffect, useState} from "react"
 import stl from './RecruitPost.module.css';
+import {API} from "../../../config";
+import axios from 'axios';
 
 function MyRecruitApplies(props) {
     const { recruitPost } = props;
     const [applies, setApplies] = useState([]);
     const [refresh, setRefresh] = useState(false);
     const formData = new FormData();
-    formData.append('recruit_ID', parseInt(recruitPost.id));
+    formData.append('recruit_ID', parseInt(recruitPost.recruitCarfullID));
 
     useEffect(() => {
         axios({
             url: `${API.RECRU_APL_LIST}`,
             method: 'post',
+            headers: {
+                Authorization: "Bearer " + localStorage.getItem("access_token")
+            },
             data: formData
             }).then(res => {
                 setApplies(res.data);
             }).catch(err => console.log(err))
-      });
+      }, []);
 
     return (
         <div className={stl.recruitListWrapper}>
             {applies.map((apply, index) => {
+                //debug
+                // console.log(apply);
                 return (
                     <>
                         <hr />
-                        <div><h2>{apply.applicant.nick}</h2></div>
+                        <div><h2>{apply.applicant.idNumber} | {apply.applicant.nick}</h2></div>
                         <div>
-                            <button onClick={AcceptApply(apply.ApplyId, refresh ,setRefresh)}>수락</button>
-                            <button onClick={DenyApply(apply.ApplyId, refresh, setRefresh)}>거절</button>
+                            <button onClick={() => AcceptApply(apply.applyId, refresh ,setRefresh)}>수락</button>
+                            <button onClick={() => DenyApply(apply.applyId, refresh, setRefresh)}>거절</button>
                         </div>
                     </>
                 );
@@ -42,12 +49,15 @@ function AcceptApply(applyID, refresh, setRefresh) {
     formData_applyID.append('recruit_ID', parseInt(applyID));
 
     axios({
-        url: `${API.RECRU_APL_ACCEPT}`,
+        url: `/withcar/recruit/accept`,
         method: 'post',
+        headers: {
+            Authorization: "Bearer " + localStorage.getItem("access_token")
+        },
         data: formData_applyID
         }).then(res => {
             //setApplies(res.data);
-            setRefresh(!refresh);
+            // setRefresh(!refresh);
         }).catch(err => console.log(err))
 }
 
@@ -56,12 +66,15 @@ function DenyApply(applyID, refresh, setRefresh) {
     formData_applyID.append('recruit_ID', parseInt(applyID));
 
     axios({
-        url: `${API.RECRU_APL_DENY}`,
+        url: `/withcar/recruit/deny`,
         method: 'post',
+        headers: {
+            Authorization: "Bearer " + localStorage.getItem("access_token")
+        },
         data: formData_applyID
         }).then(res => {
            // setApplies(res.data);
-           setRefresh(!refresh);
+        //    setRefresh(!refresh);
         }).catch(err => console.log(err))
 }
 
